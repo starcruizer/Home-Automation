a61da8ad793b97240ee6a51326030177cdc75a6d
StarCruizer <shakebameen@gmail.com>
2017-12-26 01:38:55 +0530

    Added Initialmodel Migration

---
+++ Home Automation/Home Automation/Models/Genre.cs
@@ -0,0 +1,8 @@
+﻿namespace Home_Automation.Models
+{
+    public class Genre
+    {
+        public byte Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Home Automation/Home Automation/Models/Gig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Home_Automation.Models
+{
+    public class Gig
+    {
+        public IdentityUser Artist { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Venue { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
