982bca06435a88ea2a13015d1e77d0228e3b9e6a
StarCruizer <shakebameen@gmail.com>
2017-12-26 02:33:57 +0530

    Created gig Model

--- Home Automation/Home Automation/Models/Genre.cs
+++ Home Automation/Home Automation/Models/Genre.cs
@@ -1,8 +1,13 @@
-﻿namespace Home_Automation.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Home_Automation.Models
 {
     public class Genre
     {
         public byte Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
     }
 }
--- Home Automation/Home Automation/Models/Gig.cs
+++ Home Automation/Home Automation/Models/Gig.cs
@@ -1,16 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations;
 
 namespace Home_Automation.Models
 {
     public class Gig
     {
-        public IdentityUser Artist { get; set; }
+        public int Id { get; set; }
+        [Required]
+        public ApplicationUser Artist { get; set; }
+
         public DateTime DateTime { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Venue { get; set; }
+
+        [Required]
         public Genre Genre { get; set; }
     }
 }
