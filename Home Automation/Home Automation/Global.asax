cb704c6d2ce0f26801179191b8579eb813cee1f0
StarCruizer <shakebameen@gmail.com>
2017-12-26 01:23:43 +0530

    Removed GigHub and added Home Autoamtion

---
+++ Home Automation/Home Automation/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Home_Automation.MvcApplication" Language="C#" %>
---
+++ Home Automation/Home Automation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Home_Automation.Startup))]
+namespace Home_Automation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
