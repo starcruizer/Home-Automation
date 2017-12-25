06fd4e0fac1a52fce85e421ec1ae3626135b544c
StarCruizer <shakebameen@gmail.com>
2017-12-26 03:39:09 +0530

    Created Add a Gig Form

---
+++ Home Automation/Home Automation/Controllers/GigsController.cs
@@ -0,0 +1,26 @@
+﻿using Home_Automation.Models;
+using Home_Automation.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Home_Automation.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ Home Automation/Home Automation/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Home_Automation.Models;
+using System.Collections.Generic;
+
+namespace Home_Automation.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
